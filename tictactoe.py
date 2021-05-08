4aefbbea24f450d0dc42d9f2faf72a742504ce9a
Restyled.io <commits@restyled.io>
2021-05-08 08:30:50 +0000

    Restyled by yapf

--- tictactoe.py
+++ tictactoe.py
@@ -42,11 +42,8 @@ class Board:
                     print("This cell is occupied! Choose another one!")
                     continue
                 break
-            self.config = (
-                self.config[: (3 - y) * 3 + x - 1]
-                + player
-                + self.config[(3 - y) * 3 + x :]
-            )
+            self.config = (self.config[:(3 - y) * 3 + x - 1] + player +
+                           self.config[(3 - y) * 3 + x:])
         else:
             print('Making move level "%s"' % difficulty)
             if difficulty == "unbeatable":
@@ -56,25 +53,20 @@ class Board:
                 difficulty = "easy"
                 for c_index, c in enumerate(self.config):
                     if c == " " and ("%s wins" % player) == self.get_state(
-                        self.config[:c_index] + player + self.config[c_index + 1 :]
-                    ):
-                        self.config = (
-                            self.config[:c_index] + player + self.config[c_index + 1 :]
-                        )
+                            self.config[:c_index] + player +
+                            self.config[c_index + 1:]):
+                        self.config = (self.config[:c_index] + player +
+                                       self.config[c_index + 1:])
                         difficulty = "medium"
                         break
                 else:
                     for c_index, c in enumerate(self.config):
-                        if c == " " and ("%s wins" % opponent) == self.get_state(
-                            self.config[:c_index]
-                            + opponent
-                            + self.config[c_index + 1 :]
-                        ):
-                            self.config = (
-                                self.config[:c_index]
-                                + player
-                                + self.config[c_index + 1 :]
-                            )
+                        if c == " " and ("%s wins" %
+                                         opponent) == self.get_state(
+                                             self.config[:c_index] + opponent +
+                                             self.config[c_index + 1:]):
+                            self.config = (self.config[:c_index] + player +
+                                           self.config[c_index + 1:])
                             difficulty = "medium"
                             break
 
@@ -82,7 +74,7 @@ class Board:
                 p = random.randrange(9)
                 while self.config[p] != " ":
                     p = random.randrange(9)
-                self.config = self.config[:p] + player + self.config[p + 1 :]
+                self.config = self.config[:p] + player + self.config[p + 1:]
 
     def print(self):
         print("  ---------")
@@ -100,16 +92,16 @@ class Board:
         for player in "XO":
             # row win:
             for row in range(3):
-                if all(c == player for c in config[3 * row : 3 * (row + 1)]):
+                if all(c == player for c in config[3 * row:3 * (row + 1)]):
                     return "%s wins" % player
             # column win:
             for column in range(3):
                 if all(c == player for c in config[column::3]):
                     return "%s wins" % player
             # diagonal win:
-            if all(c == player for c in config[::4]) or all(
-                c == player for c in config[2:7:2]
-            ):
+            if all(c == player
+                   for c in config[::4]) or all(c == player
+                                                for c in config[2:7:2]):
                 return "%s wins" % player
         # Moved to after the win check
         # Reason: If the last move decides the victor, it should not be declared as Draw
@@ -122,7 +114,8 @@ class Board:
         unbeatable_config = self.config
         for c_index, c in enumerate(self.config):
             if c == " ":
-                new_config = self.config[:c_index] + player + self.config[c_index + 1 :]
+                new_config = self.config[:c_index] + player + self.config[
+                    c_index + 1:]
                 new_rank = self.get_rank(new_config, player)
                 if new_rank > rank:
                     rank = new_rank
@@ -149,16 +142,16 @@ class Board:
                     rank = max(
                         rank,
                         self.get_rank(
-                            config[:c_index] + turn + config[c_index + 1 :], player
-                        ),
+                            config[:c_index] + turn + config[c_index + 1:],
+                            player),
                     )
                 else:
                     # Minimize for turns made by opponent
                     rank = min(
                         rank,
                         self.get_rank(
-                            config[:c_index] + turn + config[c_index + 1 :], player
-                        ),
+                            config[:c_index] + turn + config[c_index + 1:],
+                            player),
                     )
         return rank
 
@@ -184,17 +177,16 @@ if __name__ == "__main__":
 
     while True:
         print("Available modes: " + ", ".join(player_modes))
-        command = (
-            input("Enter mode for Player %d [%s]: " % (player_num, default_mode))
-            or default_mode
-        )
+        command = (input("Enter mode for Player %d [%s]: " %
+                         (player_num, default_mode)) or default_mode)
 
         if command == "exit":
             break
 
         if command not in player_modes:
             print("Bad parameters!")
-            print("Enter one of the following mode: " + ", ".join(player_modes))
+            print("Enter one of the following mode: " +
+                  ", ".join(player_modes))
 
             continue
 
