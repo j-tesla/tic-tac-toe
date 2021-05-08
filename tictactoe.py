419c07ef6141cc0a3e2d1d5bf70be97effc5a5ba
Restyled.io <commits@restyled.io>
2021-05-08 08:30:45 +0000

    Restyled by autopep8

--- tictactoe.py
+++ tictactoe.py
@@ -100,7 +100,7 @@ class Board:
                     return "%s wins" % player
             # diagonal win:
             if all(c == player for c in config[::4]) or all(
-                c == player for c in config[2:7:2]):
+                    c == player for c in config[2:7:2]):
                 return "%s wins" % player
         # Moved to after the win check
         # Reason: If the last move decides the victor, it should not be declared as Draw
